941547fc63fde1396434ebcf8796654bf4bd5422
RAlanWright <31636206+RAlanWright@users.noreply.github.com>
2018-11-02 13:45:05 -0500

    Added .gitignore

--- pdf/views/resume.php
+++ pdf/views/resume.php
@@ -14,7 +14,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
-<body>
+<body style="margin: auto;">
   <div class="container" style="display: flex;max-height: 100%;margin: auto;">
     <div class="col1" style="flex: 3;border-bottom-color:10px #638DFD;">
       <section style="min-height:50px;padding:.5em;border-bottom:1px solid #638DFD;">
@@ -22,32 +22,33 @@
         <p>Seeking an entry level position where I can utilize recent training to deliver <br>value as a <strong>Software Developer</strong>.</p>
       </section>
       <section style="min-height:50px;padding:.5em;">
-        <h3 style="color:#2F58C6">Experience</h3>
-        <h4>Apex Covantage, Memphis TN - Technician <br>05/2017 - 03/27/2018</h4>
+        <h4 style="color:#2F58C6">Experience</h4>
+        <h5>Apex Covantage, Memphis TN - Technician <br>05/2017 - 03/27/2018</h5>
         <p>Provided
           excellent customer service while ensuring quality installations.</p>
-        <h4>Stay Safe Systems, Memphis TN - Technician <br>03/2017 - 05/2017</h4>
+        <h5>Stay Safe Systems, Memphis TN - Technician <br>03/2017 - 05/2017</h5>
         <p>Duties included the wiring of alarm and camera systems, routing cable in homes/businesses, troubleshooting security system software, repairing and installing security
           gates.</p>
       </section>
 
       <section style="min-height:50px;padding:.5em;">
-        <h3 style="color:#2F58C6">Education</h3>
+        <h4 style="color:#2F58C6">Education</h4>
         <p>
-          <h4>Tech901 - Code 1.0 Memphis, TN <br>08/2017 - 12/2017</h4> Formal training in Python, C, HTML/CSS, and Javascript Curriculum based on Harvard edX CS50 platform.
+          <h5>Tech901 - Code 1.0 Memphis, TN <br>08/2017 - 12/2017</h5> Formal training in Python, C, HTML/CSS, and Javascript Curriculum based on Harvard edX CS50 platform.
         </p>
         <p>
-          <h4>Tech901 - IT Foundations Memphis, TN <br>01/2018 - 04/2018</h4> Formal training for CompTIA IT Fundamentals & A+ certifications.
+          <h5>Tech901 - IT Foundations Memphis, TN <br>01/2018 - 04/2018</h5> Formal training for CompTIA IT Fundamentals & A+ certifications.
         </p>
       </section>
       <section style="min-height:50px;padding:.5em;">
-        <h3 style="color:#2F58C6">Side Projects</h3>
+        <h4 style="color:#2F58C6">Side Projects</h4>
         <p><strong>TripPlanner.java:</strong> Takes user input and calculates budget based on duration of trip, currency conversion, and displays time difference.</p>
         <p><strong>OddsAndEvens.java:</strong> User input and randomized computer choice to see who wins a game of Odds and Evens.</p>
+        <p><strong>CV_Convert:</strong> Takes HTML/CSS and runs it through PhantomJS using PHP in order to generate a PDF file.</p>
       </section>
 
       <section style="min-height:50px;padding:.5em;">
-        <h3 style="color:#2F58C6">Volunteer Work</h3>
+        <h4 style="color:#2F58C6">Volunteer Work</h4>
         <p><strong>Worklist Web App:</strong> Contributed in building a mobile first Rails web application for Overton Park Conservancy during Givecamp 2018.</p>
       </section>
 
@@ -56,13 +57,13 @@
 
     <div class="col2" style="flex:1;border-left:1px solid #638DFD;">
       <section style="min-height:50px;padding:.5em;">
-        <h3 style="color:#2F58C6">Contact</h3>
+        <h4 style="color:#2F58C6">Contact</h4>
         <p>(901)500-9517</p>
         <p>robertwright2@fedex.com</p>
         <p>Cordova, TN 38018</p>
       </section>
       <section style="min-height:50px;padding:.5em;">
-        <h3 style="color:#2F58C6">Skills</h3>
+        <h4 style="color:#2F58C6">Skills</h4>
         <ul>
           <li>C</li>
           <li>HTML5/CSS3</li>
@@ -74,7 +75,7 @@
         </ul>
       </section>
       <section style="min-height:50px;padding:.5em;">
-        <h3 style="color:#2F58C6">Tools</h3>
+        <h4 style="color:#2F58C6">Tools</h4>
         <ul>
 
           <li>Git</li>
@@ -88,7 +89,7 @@
         </ul>
       </section>
       <section style="min-height:50px;padding:.5em;">
-        <h3 style="color:#2F58C6">Certifications</h3>
+        <h4 style="color:#2F58C6">Certifications</h4>
         <ul>
         <li>CompTIA IT Fundamentals</li>
         <li>CompTIA A+</li>
